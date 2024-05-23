fd83b838c7599ccde89181af090bfcebbb2af298
Wittchen <swittche@ipb-halle.de>
2024-05-23 10:34:46 +0200

    Phase 8: NoSuchEmployeeException created and class EmployeeIncorrectData for setting the exception information also created. Both classes are added in new method handleException in MyRestController. Also -parameters argument added to compiler plugin in maven in order to work with (@PathVariable int id) annotation.

--- src/main/java/com/sergei/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/sergei/spring/rest/controller/MyRESTController.java
@@ -1,15 +1,19 @@
 package com.sergei.spring.rest.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sergei.spring.rest.entity.Employee;
+import com.sergei.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.sergei.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.sergei.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-
 public class MyRESTController {
 
     @Autowired
@@ -23,10 +27,23 @@ public class MyRESTController {
     }
 
     @GetMapping("/employees/{id}")
-    public Employee getEmployee(@PathVariable int id) {
+    public Object getEmployee(@PathVariable int id) throws JsonProcessingException {
         Employee employee = employeeService.getEmployee(id);
-
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID= " + id + "in Database!");
+        }
         return employee;
     }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception) {
+
+        //creating an object of EmployeeIncorrectData and setting an error message
+        EmployeeIncorrectData employeeIncorrectData = new EmployeeIncorrectData();
+        employeeIncorrectData.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(employeeIncorrectData, HttpStatus.NOT_FOUND);
+    }
+
 
 }
